5de1911a05b3fbc39f3346803de32aef2b74bebe
PizzaPoot <viira.mihkel@gmail.com>
2024-05-11 22:57:47 +0300

    Finished it ig
    Scroll_frame still a bit broken when loading while not at top

--- RunViewer.py
+++ RunViewer.py
@@ -10,42 +10,49 @@ def loadsave(filename):
     file = open(f"C:/Program Files (x86)/Steam/steamapps/common/The Binding of Isaac Rebirth/data/tboi mod/{filename}", encoding="utf-8")
     savedata = json.load(file)
     file.close()
-    saves_frame.grid_remove()
     loadinfopage(savedata)
 
 def loadfile():
     file = fd.askopenfile(mode="r", filetypes=[("DAT files", "*.dat")])
     savedata = json.load(file)
-    print(savedata)
     file.close()
-    saves_frame.grid_remove()
     loadinfopage(savedata)
 
-saves_frame = tk.CTkFrame(app, width=500, height=500)
-saves_frame.grid(row=0, column=0, padx=20, pady=20)
-saves_frame.pack_propagate(False)
-tk.CTkLabel(saves_frame, text="Select a save file\nfrom your TBoI folder", font=("Arial", 30)).pack(pady=15, side="top")
-tk.CTkButton(saves_frame, text="Browse", command=loadfile, width=200, height=35).pack(pady=20, side="bottom") 
-tk.CTkLabel(saves_frame, text="Or pick a save file from your computer", font=("Arial", 20)).pack(pady=5, side="bottom")
-
-for i in os.listdir("C:/Program Files (x86)/Steam/steamapps/common/The Binding of Isaac Rebirth/data/tboi mod"):
-    button_event = lambda i=i: loadsave(i)
-    button = tk.CTkButton(saves_frame, text=i, command=button_event ,width=200, height=35)
-    button.pack()
+def loaddefaultpage():
+    for widget in scroll_frame.winfo_children():
+        widget.destroy()
+    infotitle.configure(text="Run Viewer\nBy PizzaPoot", font=("Arial", 30))
+    infotext.configure(text="Stupid ahh program\n\nIdk what tf to write here\n\nSome stupid shit XD")
 
 scroll_frame = tk.CTkScrollableFrame(app, width=200, height=500)
-info_frame = tk.CTkFrame(app, width=500, height=500)
-infotitle = tk.CTkLabel(info_frame, text="Info", font=("Arial", 30))
+info_frame = tk.CTkFrame(app, width=500, height=510)
+infotitle = tk.CTkLabel(info_frame, text="", font=("Arial", 30))
 infotext = tk.CTkLabel(info_frame, text="", font=("Arial", 22), anchor="w", justify="left")
-tempscroll_frame = tk.CTkFrame(app, width=200, height=500)
+tempscroll_frame = tk.CTkFrame(app, width=220, height=510)
 progress_frame = tk.CTkFrame(info_frame, width=500, height=500)
 progressbar = tk.CTkProgressBar(progress_frame, mode='determinate')
 progresstext1 = tk.CTkLabel(progress_frame, text="0%", anchor="w")
 progresstext2 = tk.CTkLabel(progress_frame, text="0/0", anchor="e")
-#{"bosscount":0,"roomsentered":2,"floor":"Basement I","runid":2,"exited":false,"totalruntime":56587,"enemyCount":6,"diedEnding":true,"dateended":"20240508130847"}
-def loadinfo(index, savedata):
-    if index == 3:
-        reloadsave()
+buttons_frame = tk.CTkFrame(app, width=1000, height=50)
+buttons_frame.grid(row=0, column=0, padx=20, pady=20, columnspan=2)
+info_frame.grid(row=1, column=1, padx=20, pady=20, sticky="w")
+info_frame.grid_propagate(False)
+tempscroll_frame.grid(row=1, column=0, padx=20, pady=20, sticky="w")
+infobutton = tk.CTkButton(buttons_frame, text="Info", command=loaddefaultpage, width=125, height=35)
+infobutton.grid(padx=10, pady=15, row=0, column=0)
+browsebutton = tk.CTkButton(buttons_frame, text="Browse", command=loadfile, width=125, height=35)
+browsebutton.grid(padx=10, pady=15, row=0, column=1)
+infotitle.place(relx=0.5, rely=0.05, anchor="n")
+infotext.place(relx=0.05, rely=0.5, anchor="w")
+loaddefaultpage()
+
+for num, i in enumerate(os.listdir("C:/Program Files (x86)/Steam/steamapps/common/The Binding of Isaac Rebirth/data/tboi mod")):
+    button_event = lambda i=i: loadsave(i)
+    button = tk.CTkButton(buttons_frame, text=i, command=button_event ,width=125, height=35)
+    button.grid(padx=10, pady=15, row=0, column=num+2)
+
+
+def loadsaveinfo(index, savedata):
     infotitle.configure(text=f"Run #{index} stats")
     if savedata[index-1]["exited"]:
         ended = "Exited the run"
@@ -86,14 +93,16 @@ def converttime(ms):
     return f"{hours}h {minutes%60}m {seconds%60}s"
 
 def loadinfopage(savedata):
-    info_frame.grid(row=0, column=1, padx=20, pady=20)
-    info_frame.grid_propagate(False)
+    for widget in scroll_frame.winfo_children():
+        widget.destroy()
+    infotitle.configure(text="")
+    infotext.configure(text="")
 
-    tempscroll_frame.grid(row=0, column=0, padx=20, pady=20)
     scroll_frame.grid_remove()
     progress_frame.place(relx=0.5, rely=0.5, anchor="center", )
+    tempscroll_frame.grid(row=1, column=0, padx=20, pady=20, sticky="w")
 
-    progressbar.grid(row=0, column=0, sticky='ew')
+    progressbar.grid(row=1, column=0, sticky='ew')
 
     progresstext1.grid(row=1, column=0, sticky='w')
     progresstext2.grid(row=1, column=0, sticky='e')
@@ -105,7 +114,7 @@ def loadinfopage(savedata):
             buttontime = "- " + convertdate(savedata[i-1]["datestarted"])[0:10] + " " + converttime(savedata[i-1]["totalruntime"])
         else:
             buttontime = "- " + converttime(savedata[i-1]["totalruntime"])
-        button_event = lambda i=i: loadinfo(i, savedata)
+        button_event = lambda i=i: loadsaveinfo(i, savedata)
         button = tk.CTkButton(scroll_frame, text=f"Run {i} {buttontime}", command=button_event, width=200, height=35)
         button.pack()
 
@@ -114,9 +123,7 @@ def loadinfopage(savedata):
         progresstext2.configure(text=f"{i}/{len(savedata)}")
         app.update_idletasks()
 
-    scroll_frame.grid(row=0, column=0, padx=20, pady=20)
-    infotitle.place(relx=0.5, rely=0.05, anchor="n")
-    infotext.place(relx=0.05, rely=0.5, anchor="w")
+    scroll_frame.grid(row=1, column=0, padx=20, pady=20, sticky="w")
 
     tempscroll_frame.grid_remove()
     progress_frame.place_forget()
